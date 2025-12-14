eec03c4264ea5bba0293c533dc0b37c6986868aa
hariniis250314-stack <harinii.s250314@gmail.com>
2025-12-14 12:32:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Fraud & Anomaly Detection System",
+    layout="wide"
+)
+
+st.title("🚨 AI Fraud & Anomaly Detection System")
+st.write("Detecting suspicious transactions using advanced ML and DL models.")
