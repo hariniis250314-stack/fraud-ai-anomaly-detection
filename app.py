7e463ab07bf8cbcc1a587655a0a9111b7e442432
hariniis250314-stack <harinii.s250314@gmail.com>
2025-12-14 12:36:32 +0530

    Added CSV upload functionality

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
-    page_title="AI Fraud & Anomaly Detection System",
+    page_title="AI Fraud Detection System",
     layout="wide"
 )
 
 st.title("🚨 AI Fraud & Anomaly Detection System")
-st.write("Detecting suspicious transactions using advanced ML and DL models.")
+st.write("Upload transaction data to detect suspicious behavior.")
+
+uploaded_file = st.file_uploader(
+    "Upload Transaction CSV File",
+    type=["csv"]
+)
